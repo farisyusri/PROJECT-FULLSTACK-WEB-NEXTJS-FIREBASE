dec023d075356f3193beae4252e06369291595df
Faris Rahman <farisrhmn15@gmail.com>
2024-12-01 13:18:45 +0700

    Added new files

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+import "./dashboard.css";
+
+function DashboardPage() {
+  return (
+    <div className="dashboard-container">
+      {" "}
+      {}
+      <h1 className="dashboard-title">Welcome to the Dashboard!</h1>
+      <p className="dashboard-description">You have successfully signed in.</p>
+      {}
+    </div>
+  );
+}
+
+export default DashboardPage;
